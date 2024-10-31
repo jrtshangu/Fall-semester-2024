5cbdc2188358d9ef2913cf6f275a1dcc38b8e683
Jean Robert <weavrtshangu@gmail.com>
2024-10-31 20:18:41 +0100

    Creating a project

---
+++ projects/scripts/script.js
@@ -0,0 +1,14 @@
+
+
+let roadName = document.getElementById("road-name");
+let tsQ1 = document.getElementById("ts-q1");
+
+let tsQ2 = document.getElementById("ts-q2");
+let tsQ3 = document.getElementById("ts-q3");
+let tsTj  = document.getElementById("ts-tj");
+
+function chooseB(){
+    let areaName = document.getElementById("area-result");
+    let tsBitabe = document.getElementById("ts-bitabe");
+    areaName.innerHTML = "Masina Bitabe";
+}
