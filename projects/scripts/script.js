261d45174c24dd855382960cc4513719d3465d78
Jean Robert <weavrtshangu@gmail.com>
2024-11-04 21:38:46 +0100

    adding some scripts

--- projects/scripts/script.js
+++ projects/scripts/script.js
@@ -1,8 +1,17 @@
 
-function register(){
+function signUp(){
+    let create = document.getElementById("register");
+    let connect = document.getElementById("connect");
+
+    create.style.display="block";
+    connect.style.display= "none";
 
 }
 
 function connect(){
+    let create = document.getElementById("register");
+    let connect = document.getElementById("connect");
 
+    create.style.display="none";
+    connect.style.display= "block";
 }
