74a6a27d5df462d68f8ec89a3ad8b2faf136236b
Jean Robert <weavrtshangu@gmail.com>
2024-11-04 21:22:35 +0100

    adding some scripts

--- projects/scripts/script.js
+++ projects/scripts/script.js
@@ -1,9 +1,8 @@
 
+function register(){
 
-//let roadName = document.getElementById("road-name");
-//let tsQ1 = document.getElementById("ts-q1");
+}
 
-//let tsQ2 = document.getElementById("ts-q2");
-//let tsQ3 = document.getElementById("ts-q3");
-//let tsTj  = document.getElementById("ts-tj");
+function connect(){
 
+}
